bc7443b4cdaebc523389b5f69aa16687d2e87bd0
Sweta2706 <124232607+Sweta2706@users.noreply.github.com>
2023-10-28 23:27:59 +0530

    Initial commit

---
+++ src/routes/Home.js
@@ -0,0 +1,27 @@
+import Destination from "../components/Destination";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Trip from "../components/Trip";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero
+        cName="hero"
+        heroImg="https://images.unsplash.com/photo-1621103332371-9c95d964b4d6?auto=format&fit=crop&q=80&w=1530&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        title="Your Journey Your Story"
+        text="Choose Your Favourite Destination."
+        buttonText="Travel Plan"
+        url="/"
+        btnClass="show"
+      />
+      <Destination />
+      <Trip />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
